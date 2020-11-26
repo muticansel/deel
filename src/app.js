e05adb72d84a95126880fa12b1044efd230b6789
muticansel <cancel.muti@gmail.com>
2020-11-27 01:27:35 +0300

    client

--- src/app.js
+++ src/app.js
@@ -8,6 +8,12 @@ app.use(bodyParser.json());
 app.set('sequelize', sequelize)
 app.set('models', sequelize.models)
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 
 /**
  * FIX ME!
